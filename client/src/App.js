4188957b806a96f35860d85ef55ae52307effc59
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-12-15 16:18:42 -0500

    css updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,95 +1,77 @@
-import { useState, useEffect, useHistory } from 'react'
-import {Route, Routes, Navigate } from 'react-router-dom'
-import About from './components/About';
-import Home from './components/Home';
-import Layout from './components/Layout';
-import Nav from './components/Nav';
-import CreatePrint from './screens/Prints/CreatePrint';
-import EditPrint from './screens/Prints/EditPrint';
-import Prints from './screens/Prints/Prints';
-import SinglePrint from './screens/Prints/SinglePrints';
-import SignIn from './screens/SignUp&In/SignIn';
-import SignUp from './screens/SignUp&In/SignUp';
-import {loginUser, registerUser, verifyUser, removeToken} from './services/Authentication'
+import { useState, useEffect, useHistory } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Nav from "./components/Nav";
+import CreatePrint from "./screens/Prints/CreatePrint";
+import EditPrint from "./screens/Prints/EditPrint";
+import Prints from "./screens/Prints/Prints";
+import SinglePrint from "./screens/Prints/SinglePrints";
+import SignIn from "./screens/SignUp&In/SignIn";
+import SignUp from "./screens/SignUp&In/SignUp";
+import {
+  loginUser,
+  registerUser,
+  verifyUser,
+  removeToken,
+} from "./services/Authentication";
 function App() {
-  const [user, setUser] = useState(null)
-  const [open, setOpen] = useState(false)
+  const [user, setUser] = useState(null);
+  const [open, setOpen] = useState(false);
 
   // let history = useHistory();
-console.log(user)
+  console.log(user);
   const handleOpen = () => {
-    setOpen(true)
-  }
-  const handleClose= () => {
-    setOpen(false)
-  }
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   // create useEffect to fetch verified user
   useEffect(() => {
     const handleVerify = async () => {
       const userData = await verifyUser();
-      setUser(userData)
-    }
+      setUser(userData);
+    };
     handleVerify();
-  }, [])
+  }, []);
   const handleLogin = async (formData) => {
     const userData = await loginUser(formData);
     setUser(userData);
-  }
+  };
 
   const handleRegister = async (formData) => {
     const userData = await registerUser(formData);
     setUser(userData);
-    
   };
   const handleLogout = () => {
     setUser(null);
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
     removeToken();
   };
   return (
     <Layout user={user} handleLogout={handleLogout}>
-    <div className="App">
-      {/* create Routes */}
-      <Routes>
-        <Route
-          exact path='/'
-          element={<Home/>}
-        />
-        <Route
-          path='/sign-in'
-          element={<SignIn handleLogin={handleLogin}/>}
+      <div className="App">
+        {/* create Routes */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            path="/sign-in"
+            element={<SignIn handleLogin={handleLogin} />}
           />
-        <Route
-          path='/sign-up'
-          element={<SignUp handleRegister={handleRegister}/>}
+          <Route
+            path="/sign-up"
+            element={<SignUp handleRegister={handleRegister} />}
           />
-        <Route
-          path='/print/:id'
-          element={<SinglePrint/>}
-          />
-        <Route
-          path='/prints'
-          element={<Prints/>}
-          />
-        <Route
-          path='/print-edit/:id'
-          element={<EditPrint/>}
-          />
-        <Route
-            path='/create-print'
-          element={<CreatePrint/>}
-          />
-        <Route
-          path='/about-us'
-          element={<About/>}
-          />
-      </Routes>
-      {/* {<SignUp
-        handleOpen={handleOpen}
-        handleClose={handleClose}
-      />} */}
-    </div>
-      </Layout>
+          <Route path="/print/:id" element={<SinglePrint />} />
+          <Route path="/prints" element={<Prints />} />
+          <Route path="/print-edit/:id" element={<EditPrint />} />
+          <Route path="/create-print" element={<CreatePrint />} />
+          <Route path="/about-us" element={<About />} />
+        </Routes>
+      </div>
+    </Layout>
   );
 }
 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,66 +1,86 @@
-import { Link, NavLink, useLocation, useNavigate} from 'react-router-dom'
-import NavCSS from './components css/Nav.css'
-import { slide as Menu } from 'react-burger-menu'
-const  Nav = (props) => {
-  let { user, handleLogout } = props
-  let navigate = useNavigate()
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import NavCSS from "./components css/Nav.css";
+import { slide as Menu } from "react-burger-menu";
+const Nav = (props) => {
+  let { user, handleLogout } = props;
+  let navigate = useNavigate();
   const authenticatedOptions = (
     <>
-        <Link className="nav-links" to="/" onClick={() => handleLogout()}>SIGN OUT</Link>
+      <Link className="nav-links" to="/" onClick={() => handleLogout()}>
+        SIGN OUT
+      </Link>
     </>
-  )
+  );
   const unauthenticatedOptions = (
     <>
-        <Link className="nav-links" to="/sign-up"><div className='nav-links'>SIGN UP</div></Link>
-        <Link className="nav-links" to="/sign-in"><div className='nav-links'>SIGN IN</div></Link>
+      <Link className="nav-links" to="/sign-up">
+        <div className="nav-links">SIGN UP</div>
+      </Link>
+      <Link className="nav-links" to="/sign-in">
+        <div className="nav-links">SIGN IN</div>
+      </Link>
     </>
-  )
+  );
   // HBGR MENU OPTIONS
   /*-------Menu Options------*/
-const authenticatedOptionsMenu = (
-  <>
-      <Link className="burger-item" className="link" to="/sign-out">SIGN OUT</Link>
-  </>
-)
+  const authenticatedOptionsMenu = (
+    <>
+      <Link className="burger-item" className="link" to="/sign-out">
+        SIGN OUT
+      </Link>
+    </>
+  );
 
-const unauthenticatedOptionsMenu = (
-  <>
-      <Link className="burger-item" className="link" to="/sign-up">SIGN UP</Link>
-      <Link className="link" to="/sign-in">SIGN IN</Link>
-  </>
-)
-const alwaysOptionsMenu = (
-  <>
-    <Link className="burger-item" className="link" to="/prints">PRINTS</Link>
-    <Link className="burger-item" className="link" to="/about-us">ABOUT</Link>
-  </>
-)
+  const unauthenticatedOptionsMenu = (
+    <>
+      <Link className="burger-item" className="link" to="/sign-up">
+        SIGN UP
+      </Link>
+      <Link className="link" to="/sign-in">
+        SIGN IN
+      </Link>
+    </>
+  );
+  const alwaysOptionsMenu = (
+    <>
+      <Link className="burger-item" className="link" to="/prints">
+        PRINTS
+      </Link>
+      <Link className="burger-item" className="link" to="/about-us">
+        ABOUT
+      </Link>
+    </>
+  );
   return (
-    <div >
+    <div>
       <nav>
-        <div className='nav-wrapper'>
-          <Link to='/' classname='nav-logo'><img className='nav-logo' src='https://i.imgur.com/oQZa4K9.png' /></Link>
-          <div className='nav-links'>
-        <Link to='/prints'><div className='nav-links'>PRINTS</div></Link>
-        <Link to='/about-us'><div className='nav-links'>ABOUT US</div></Link>
-        
-           {/* {alwaysOptions} */}
-        {user ? authenticatedOptions : unauthenticatedOptions}
-        {user && <div className="link-welcome">Welcome, {user.email}</div>}
+        <div className="nav-wrapper">
+          <Link to="/" classname="nav-logo">
+            <img className="nav-logo" src="https://i.imgur.com/oQZa4K9.png" />
+          </Link>
+          <div className="nav-links">
+            <Link to="/prints">
+              <div className="nav-links">PRINTS</div>
+            </Link>
+            <Link to="/about-us">
+              <div className="nav-links">ABOUT US</div>
+            </Link>
 
-        </div>
+            {/* {alwaysOptions} */}
+            {user ? authenticatedOptions : unauthenticatedOptions}
+            {user && <div className="link-welcome">Welcome, {user.email}</div>}
+          </div>
         </div>
       </nav>
-      <nav className='moblie-nav'>
-        <Menu className='moblie-nav'>
+      <nav className="moblie-nav">
+        <Menu className="moblie-nav">
           {alwaysOptionsMenu}
-        {user ? authenticatedOptionsMenu : unauthenticatedOptionsMenu}
-        {user && <div className="link-welcome">Welcome, {user.email}</div>}
-
+          {user ? authenticatedOptionsMenu : unauthenticatedOptionsMenu}
+          {user && <div className="link-welcome">Welcome, {user.email}</div>}
         </Menu>
       </nav>
     </div>
-  )
-}
+  );
+};
 
 export default Nav;
